7a86a389329acb5cd6aef6ea192b642012876285
Nilambar Sharma <nilambar@outlook.com>
2026-03-08 14:16:40 +0545

    Update tokens

--- Formula/kase.rb
+++ Formula/kase.rb
@@ -4,8 +4,8 @@
 class Kase < Formula
   desc "CLI for converting, detecting, and applying string case (Unix-friendly, pipe-safe)"
   homepage "https://github.com/ernilambar/kase"
-  url "https://github.com/ernilambar/kase/archive/refs/tags/0.1.0.tar.gz"
-  sha256 "b3b71120a27983a0391a105f23d2a686d81fcba6ecfed7b899b0902ea5863c43"
+  url "https://github.com/ernilambar/kase/archive/refs/tags/0.1.1.tar.gz"
+  sha256 "4a6a3e9694ef43f1c6b583682e4fb7f6fb625f25a30a2380f71e9a32734090d0"
   license "MIT"
 
   depends_on "go" => :build
