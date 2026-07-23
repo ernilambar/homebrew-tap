2eaabf96d9c94169fb22caa38e2612f4297db5cc
Nilambar Sharma <nilambar@outlook.com>
2026-07-23 07:24:04 +0545

    Bump glot to 1.0.2

--- Formula/glot.rb
+++ Formula/glot.rb
@@ -4,17 +4,17 @@
 class Glot < Formula
   desc "CLI tool for translating WordPress .po files using any OpenAI-compatible backend"
   homepage "https://github.com/ernilambar/glot-cli"
-  version "1.0.1"
+  version "1.0.2"
   license "MIT"
 
   on_macos do
     on_arm do
-      url "https://github.com/ernilambar/glot-cli/releases/download/v1.0.1/glot-darwin-arm64"
-      sha256 "f9d6fc11d22d638ffa17bc4d45b7a5843da56b6bcb9daf1a833e2ba4656c1d1b"
+      url "https://github.com/ernilambar/glot-cli/releases/download/v1.0.2/glot-darwin-arm64"
+      sha256 "acc7a63caf1ceea8c102e4620653947d6c31ef1580893b8d9e14502d7b67a1ac"
     end
     on_intel do
-      url "https://github.com/ernilambar/glot-cli/releases/download/v1.0.1/glot-darwin-amd64"
-      sha256 "d8d2d6c74bcebd5710fdc039613020526ad5fd07507bfb56f65b870a2aad2e12"
+      url "https://github.com/ernilambar/glot-cli/releases/download/v1.0.2/glot-darwin-amd64"
+      sha256 "0e79e8cea14233b8a0124b16106fbb474fefb980636a8fbbf01b0621f4eefffc"
     end
   end
 
