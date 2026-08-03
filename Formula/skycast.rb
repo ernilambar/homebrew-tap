02e03e487f9a07d43450c1421c1a824f5093d1a5
Nilambar Sharma <nilambar@outlook.com>
2026-08-03 11:12:04 +0545

    Bump skycast to 1.0.4

--- Formula/skycast.rb
+++ Formula/skycast.rb
@@ -4,17 +4,17 @@
 class Skycast < Formula
   desc "Terminal weather app"
   homepage "https://github.com/ernilambar/skycast"
-  version "1.0.3"
+  version "1.0.4"
   license "MIT"
 
   on_macos do
     on_arm do
-      url "https://github.com/ernilambar/skycast/releases/download/v1.0.3/skycast-darwin-arm64"
-      sha256 "1cc5cc2293d38c5ed9d6f469db6387eada6df421e40c5f90cd9e9e1bc81b32ea"
+      url "https://github.com/ernilambar/skycast/releases/download/v1.0.4/skycast-darwin-arm64"
+      sha256 "37973d2ef434f0bd2a27ca473320bdd3d7e71bfbfcf99df30416e571c6bd9969"
     end
     on_intel do
-      url "https://github.com/ernilambar/skycast/releases/download/v1.0.3/skycast-darwin-amd64"
-      sha256 "d8077ae4d326b7482fa24bee7e523e88aba19080d4ace02311b1ad707536ba3f"
+      url "https://github.com/ernilambar/skycast/releases/download/v1.0.4/skycast-darwin-amd64"
+      sha256 "5e8b6ddc43a5426c13c0e41cfc732e3e93a6af94b2bd3f933dbd8fa7f3fdd41e"
     end
   end
 
