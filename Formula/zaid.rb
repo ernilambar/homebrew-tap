35f775c884f60c829c1df9b6ddc74dc3ffa4a410
Nilambar Sharma <nilambar@outlook.com>
2026-08-10 15:28:49 +0545

    Bump zaid to 1.0.3

--- Formula/zaid.rb
+++ Formula/zaid.rb
@@ -4,17 +4,17 @@
 class Zaid < Formula
   desc "AI-powered CLI toolkit"
   homepage "https://github.com/ernilambar/zaid"
-  version "1.0.2"
+  version "1.0.3"
   license "MIT"
 
   on_macos do
     on_arm do
-      url "https://github.com/ernilambar/zaid/releases/download/v1.0.2/zaid-darwin-arm64"
-      sha256 "0e19842c9b3190a31743a65e1364e067c55c61c4724f8e8181a88b1ade93e620"
+      url "https://github.com/ernilambar/zaid/releases/download/v1.0.3/zaid-darwin-arm64"
+      sha256 "89323c2ae14f52d5fda2276c862848190c0939c32191dc6dd5d45009cc034c90"
     end
     on_intel do
-      url "https://github.com/ernilambar/zaid/releases/download/v1.0.2/zaid-darwin-amd64"
-      sha256 "52f0c02692fff0109a6ed3a1dbceffeeab0b75f1bd29d7c6d49d05a0a9504064"
+      url "https://github.com/ernilambar/zaid/releases/download/v1.0.3/zaid-darwin-amd64"
+      sha256 "b51c47c09f30399d9dd8039efd1d59c8c7fb8337bdcdf0403f8e2586c1c0ff25"
     end
   end
 
